1f284b0b0114f639856a494519a95a823ea24890
agumusfu <250542016@firat.edu.tr>
2025-11-04 11:27:52 +0300

    Add files via upload
    Ödev verileri kişisel bilgisayarda düzenlenip gönderildi

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,41 @@
 
+/*
+ * Ad Soyad: Abdullah Gümüş
+ * Öğrenci No: 250542016
+ * Tarih: 04.11.2025
+ * Açıklama: Görev 2 - Geometrik Hesaplayıcı
+ * Bu program, kullanıcıdan dairenin yarıçapını alır ve
+ * daire ile küreye ait alan, çevre, çap, hacim ve yüzey alanı
+ * hesaplamalarını yaparak sonuçları düzenli bir şekilde ekrana yazdırır.
+ */
+
+import java.util.Scanner;
+
+public class GeometrikHesap {
+    public static void main(String[] args) {
+        //Pi sayısını tanımla
+        final double PI = 3.14159;
+        //Veri girişi için scanner tanımla
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== GEOMETRIK HESAPLAYICI ===");
+        System.out.print("Dairenin yaricapini girin (cm): ");
+        double r = input.nextDouble();
+        //matematiksel formüllere uygun şekilde hesaplamalar tanımlama
+        double daireAlani = PI * r * r;
+        double daireCevresi = 2 * PI * r;
+        double daireCapi = 2 * r;
+        double kureHacmi = (4.0 / 3.0) * PI * Math.pow(r, 3);
+        double kureYuzeyAlani = 4 * PI * r * r;
+        //uygun biçimlerde sonuçları ekrana yazdırma
+        System.out.println("\nSONUCLAR:");
+        System.out.println("----------");
+        System.out.printf("Daire Alani : %.2f cm^2%n", daireAlani);
+        System.out.printf("Daire Cevresi : %.2f cm%n", daireCevresi);
+        System.out.printf("Daire Capi : %.2f cm%n", daireCapi);
+        System.out.printf("Kure Hacmi : %.2f cm^3%n", kureHacmi);
+        System.out.printf("Kure Yuzey Alani : %.2f cm^2%n", kureYuzeyAlani);
+        //scanner kapatma
+        input.close();
+    }
+}
--- MaasHesap.java
+++ MaasHesap.java
@@ -1,1 +1,74 @@
 
+/*
+ * Ad Soyad: Abdullah Gümüş
+ * Ogrenci No: 250542016
+ * Tarih: 04.11.2025
+ * Aciklama:Görev 3 - Maaş Hesaplama Sistemi*
+ * Bu program, çalışanın bilgilerini alır ve brüt maaş ile mesai saatine göre
+ * gelir, kesinti ve net maaş hesaplamalarını yapar.
+ * Hesaplanan verileri ayrıntılı ve biçimlendirilmiş olarak ekrana yazdırır.
+ */
+
+
+import java.util.Scanner;
+
+public class MaasHesap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        final double SGK_ORANI = 0.14;
+        final double GELIR_VERGISI_ORANI = 0.15;
+        final double DAMGA_VERGISI_ORANI = 0.00759;
+
+        System.out.println("====================================");
+        System.out.println("MAAS BORDROSU");
+        System.out.println("====================================");
+    //Çalışanın isim-soyisim, çalışma saati ve maaş verilerini String-Double ve int ile girmesi
+        System.out.print("Çalışan Adı:");
+        String ad = input.next();
+        System.out.print("Çalışan Soyadı:");
+        String soyad = input.next();
+        System.out.print("Aylık brüt maaş (TL): ");
+        double brutMaas = input.nextDouble();
+
+        System.out.print("Haftalık Çalışma saati: ");
+        int haftalikSaat = input.nextInt();
+
+        System.out.print("Mesai saati: ");
+        int mesaiSaat = input.nextInt();
+    //Double ile istenen hesaplamalara göre tanımlama ve hesaplamalar
+        double mesaiUcreti = (brutMaas / 160) * mesaiSaat * 1.5;
+        double toplamGelir = brutMaas + mesaiUcreti;
+
+        double sgk = toplamGelir * SGK_ORANI;
+        double gelirVergisi = toplamGelir * GELIR_VERGISI_ORANI;
+        double damgaVergisi = toplamGelir * DAMGA_VERGISI_ORANI;
+        double toplamKesinti = sgk + gelirVergisi + damgaVergisi;
+        double netMaas = toplamGelir - toplamKesinti;
+
+        double kesintiOrani = (toplamKesinti / toplamGelir) * 100;
+        double saatlikNet = netMaas / 176.0;
+        double gunlukNet = netMaas / 22.0;
+        //Ekrana verilerin çıktısını biçimli bir şekilde yazdırma
+        System.out.println("\nGELİRLER:");
+        System.out.printf("Brüt Maaş : %.2f TL%n", brutMaas);
+        System.out.printf("Mesai Ücreti (%d saat) : %.2f TL%n", mesaiSaat, mesaiUcreti);
+        System.out.println("------------------------");
+        System.out.printf("TOPLAM GELİR : %.2f TL%n", toplamGelir);
+
+        System.out.println("\nKESINTILER:");
+        System.out.printf("SGK Kesintisi (%.1f%%) : %.2f TL%n", SGK_ORANI * 100, sgk);
+        System.out.printf("Gelir Vergisi (%.1f%%) : %.2f TL%n", GELIR_VERGISI_ORANI * 100, gelirVergisi);
+        System.out.printf("Damga Vergisi (%.1f%%) : %.2f TL%n", DAMGA_VERGISI_ORANI * 100, damgaVergisi);
+        System.out.println("------------------------");
+        System.out.printf("TOPLAM KESINTI : %.2f TL%n", toplamKesinti);
+
+        System.out.printf("%nNET MAAŞ : %.2f TL%n", netMaas);
+        System.out.println("====================================");
+        System.out.printf("Kesinti Orani: %.1f%%%n", kesintiOrani);
+        System.out.printf("Saatlik Net Kazanç %.2f TL%n", saatlikNet);
+        System.out.printf("Günlük Net Kazanç %.2f TL%n", gunlukNet);
+    // Scanner kapatma
+        input.close();
+    }
+}
